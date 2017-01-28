073af7107ed80e6d69e093da8305ecf6a5a88d79
Ali Can Aydogdu <alicantx@gmail.com>
2017-01-28 17:39:58 -0600

    Initial Commit

---
+++ Parsing_File.py
@@ -0,0 +1,11 @@
+ > Parsing The File
+
+ # Here's the code we used to parse the crime_rates.csv file.
+
+    f = open("crime_rates.csv", 'r')
+    data = f.read()
+    rows = data.split('\n')
+    full_data = []
+    for row in rows:
+      split_row = row.split(",")
+      full_data.append(split_row)
