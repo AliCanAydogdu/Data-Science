18755498a6722870a445aac140d10164cb9f7e0c
Ali Can Aydogdu <alicantx@gmail.com>
2017-02-06 13:48:48 -0600

    Initial Commit

---
+++ U.S._Explore_Project.py
@@ -0,0 +1,14 @@
+f = open("US_births_1994-2003_CDC_NCHS.csv", "r")
+text = f.read()
+text.split("\n")
+print(text[0:10])
+
+def read_csv(US_births_1994-2003_CDC_NCHS.csv)
+    f = open("US_births_1994-2003_CDC_NCHS.csv", "r")
+    text = f.read()
+    text.split("\n")
+    string_list = text
+    final_list = {}
+    for var in string_list:
+        int_fields = {}
+        string_fields=
